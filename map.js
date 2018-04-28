42c6e998ceaa540c086193595f5abbb8ab71c85c
Jenna Tillotson <jet7469@rit.edu>
2018-04-28 13:19:10 -0400

    Add csv layer

--- map.js
+++ map.js
@@ -1,12 +1,14 @@
   
 require([
 	"esri/Map",
+	"esri/layers/CSVLayer",
 	"esri/views/MapView",
     "dojo/domReady!"
 ], 
-function(Map, MapView) {
+function(Map, MapView, CSVLayer) {
 	var map = new Map({
 		basemap: "streets",
+		layers: [csvLayer],
     });
 
 	var view = new MapView({
@@ -15,5 +17,31 @@ function(Map, MapView) {
     	zoom: 5,
     	center: [-64, 32]    
     });
+    
+    var csvLayer = new CSVLayer({
+    	url: "http://serenity.ist.rit.edu/~jet7469/384/Final/2017Hurricanes-maria.csv",
+    	copyright: "me",
+    	
+	});
+	
+	csvLayer.renderer = {
+        type: "simple", // autocasts as new SimpleRenderer()
+        symbol: {
+          type: "point-3d", // autocasts as new PointSymbol3D()
+          symbolLayers: [{
+            type: "icon", // autocasts as new IconSymbol3DLayer()
+            material: {
+              color: [238, 69, 0, 0.75]
+            },
+            outline: {
+              width: 0.5,
+              color: "white"
+            },
+            size: "12px"
+          }]
+        }
+      };
+      
+	map.add(csvLayer);
     
 });
