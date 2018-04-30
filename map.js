d9fddd95a3551d27992a0b8f772ad009afe1ba9a
Jenna Tillotson <jet7469@rit.edu>
2018-04-30 17:13:14 -0400

    Change to API v 3.24

--- map.js
+++ map.js
@@ -1,47 +1,34 @@
-  
 require([
-	"esri/Map",
+	"esri/map",
 	"esri/layers/CSVLayer",
-	"esri/views/MapView",
-    "dojo/domReady!"
+	"esri/symbols/SimpleMarkerSymbol",
+	"esri/renderers/UniqueValueRenderer",
+	"esri/Color",
+	"dojo/domReady!"
 ], 
-function(Map, MapView, CSVLayer) {
-	var map = new Map({
+function(Map, CSVLayer, SimpleMarkerSymbol, UniqueValueRenderer, Color) {
+	
+	var map = new Map("mapDiv", {
 		basemap: "streets",
-		layers: [csvLayer],
-    });
-
-	var view = new MapView({
-    	container: "mapDiv",
-    	map: map,
-    	zoom: 5,
-    	center: [-64, 32]    
-    });
-    
-    var csvLayer = new CSVLayer({
-    	url: "http://serenity.ist.rit.edu/~jet7469/384/Final/2017Hurricanes-maria.csv",
-    	copyright: "me",
-    	
+		zoom: 5,
+    	center: [-64, 32]
 	});
 	
-	csvLayer.renderer = {
-        type: "simple", // autocasts as new SimpleRenderer()
-        symbol: {
-          type: "point-3d", // autocasts as new PointSymbol3D()
-          symbolLayers: [{
-            type: "icon", // autocasts as new IconSymbol3DLayer()
-            material: {
-              color: [238, 69, 0, 0.75]
-            },
-            outline: {
-              width: 0.5,
-              color: "white"
-            },
-            size: "12px"
-          }]
-        }
-      };
-      
-	map.add(csvLayer);
-    
+	var defaultSymbol = new SimpleMarkerSymbol().setColor([150,150,150,0.2]);
+	var defaultColor = new Color([0,0,0,0.5]);
+	
+	var renderer = new UniqueValueRenderer(defaultSymbol, "LEVEL");
+	
+	renderer.addValue("1", new SimpleMarkerSymbol({size: 6, color: defaultColor}));
+	renderer.addValue("2", new SimpleMarkerSymbol({size: 9, color: defaultColor}));
+	renderer.addValue("3", new SimpleMarkerSymbol({size: 12, color: defaultColor}));
+	renderer.addValue("4", new SimpleMarkerSymbol({size: 15, color: defaultColor}));
+	renderer.addValue("5", new SimpleMarkerSymbol({size: 18, color: defaultColor}));
+	renderer.addValue("6", new SimpleMarkerSymbol({size: 21, color: defaultColor}));
+	renderer.addValue("7", new SimpleMarkerSymbol({size: 24, color: defaultColor}));
+	
+	var csvLayer = new CSVLayer("2017.csv");
+	csvLayer.setRenderer(renderer);
+	map.addLayer(csvLayer);
+
 });
